ebe3a304002eadad29b90c7728eebe0e26f2d8db
Jonatan <jonatan.sombie@gmail.com>
2016-03-12 21:57:34 +0000

    Add mongo collection instances dependenciy

--- package.js
+++ package.js
@@ -18,8 +18,8 @@ Package.onUse(function(api) {
   api.imply('ecmascript');
   api.imply('jonatan:smart-pub@0.0.2');
   api.imply('static-html');
-  api.imply('differential:vulcanize@3.0.0')  
-  
+  api.imply('differential:vulcanize@3.0.0') ;
+  api.imply('dburles:mongo-collection-instances@0.3.5');
 
   api.addFiles('bower.json', 'client');
   
