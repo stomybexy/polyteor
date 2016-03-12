61853a657db078b3ccae3a78ee69c7e560ad81ad
Jonatan <jonatan.sombie@gmail.com>
2016-03-12 21:11:08 +0000

    inital commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Polyteor
+
+Polymer + Meteor integration.
---
+++ package.js
@@ -0,0 +1,26 @@
+Package.describe({
+  name: 'jonatan:polyteor',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+ 
+  api.use('jonatan:bower@0.0.1');  
+  
+  api.imply('ecmascript');
+  api.imply('jonatan:smart-pub@0.0.2');
+  api.imply('static-html');  
+  
+
+  api.addFiles('bower.json', 'client');
+  
+});
+
