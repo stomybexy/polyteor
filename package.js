930512b152d5a6fdd456a88055d90be3f40f6db2
stomybexy <jonatan.sombie@gmail.com>
2016-03-20 03:29:11 +0000

    Upgrade to jonatan:bower 0.0.2
    With jonatan:bower 0.0.2, now with NO_BOWER_INSTALL env set to true, one can disable bower dependencies install, usefull for production build after vulcanize.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jonatan:polyteor',
-  version: '0.0.2_1',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'Polymer + meteor integration.',
   // URL to the Git repository containing the source code for this package.
